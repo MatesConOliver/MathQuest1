562db7ff07abd03c9f58d2e919a656fd2a93e92d
oliverozcariz <oliverozcariz@britanico-aragon.edu>
2026-01-15 10:39:10 +0000

    final commit?

--- .next/dev/static/chunks/src_app_layout_tsx_1cf6b850._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__f8e30f2f._.css",
-  "static/chunks/_4b65699a._.js"
-],
-    source: "dynamic"
-});
--- src/app/play/page.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-import dynamic from 'next/dynamic'
-
-const PlayClient = dynamic(() => import('./PlayClient'), { ssr: false })
-
-export default function PlayPage() {
-  return <PlayClient />
-}
