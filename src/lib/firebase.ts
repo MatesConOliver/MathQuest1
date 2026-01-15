1219e759ef4225e7a90c6b6171b2131485d4b318
oliverozcariz <oliverozcariz@britanico-aragon.edu>
2026-01-15 11:42:20 +0000

    error fix compilation 5

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore, collection, getDocs, doc, getDoc as getDocFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,3 +20,22 @@ const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
+// Function to get all documents from a collection
+export const getAllDocs = async (collectionName: string) => {
+  const querySnapshot = await getDocs(collection(db, collectionName));
+  const documents = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+  return documents;
+};
+
+// Function to get a single document from a collection
+export const getDoc = async (collectionName: string, docId: string) => {
+  const docRef = doc(db, collectionName, docId);
+  const docSnap = await getDocFirestore(docRef);
+  if (docSnap.exists()) {
+    return { id: docSnap.id, ...docSnap.data() };
+  } else {
+    console.log("No such document!");
+    return null;
+  }
+};
