13d64114e265f026f1d6ba93d546e7b74aaddc22
oliverozcariz <oliverozcariz@britanico-aragon.edu>
2026-01-17 07:29:49 +0000

    new map background

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -69,7 +69,7 @@ export default function MapPage() {
   return (
     <main 
       className="min-h-screen bg-cover bg-center p-4 md:p-8 transition-colors duration-300"
-      style={{ backgroundImage: "url('https://i.imgur.com/g0LhT6i.jpeg')" }}
+      style={{ backgroundImage: "url('https://firebasestorage.googleapis.com/v0/b/pokematicos.firebasestorage.app/o/The_Primordial_Equation_Backgrounds%2Fmap%20background.png?alt=media&token=9d290d1c-476e-452c-b993-4c81073b3f95')" }}
     >
       <div className="max-w-4xl mx-auto space-y-6 bg-black/50 p-5 rounded-xl">
         
