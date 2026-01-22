85cdf94da6cff8afa94d1f6d6ae1f77bb6a89422
oliverozcariz <oliverozcariz@britanico-aragon.edu>
2026-01-22 12:04:55 +0000

    locations ids in maplayout

--- src/config/mapLayout.ts
+++ src/config/mapLayout.ts
@@ -9,34 +9,34 @@ export type MapLocationMeta = {
 
 export const MAP_LOCATIONS: MapLocationMeta[] = [
   {
-    locationId: "library",
+    locationId: "bSL1XkrzgqQxqtCLNumD", //library
     x: 0.5,
     y: 0.6,
     initiallyHidden: false,
     fogType: "mist",
   },
   {
-    locationId: "algebra",
+    locationId: "NNaAatxu2L25wWhKHc6Z", //algebra
     x: 0.2,
     y: 0.3,
   },
   {
-    locationId: "functions",
+    locationId: "RRpoA4VdN9MgJaiscA0b", //functions
     x: 0.8,
     y: 0.2,
   },
   {
-    locationId: "geometry",
+    locationId: "2qsVAsgI3VhpfZ091h1c", //geometry
     x: 0.3,
     y: 0.8,
   },
   {
-    locationId: "probability",
+    locationId: "MYknm1xkQq7kWF2UTcNJ", //prob and statistics
     x: 0.7,
     y: 0.8,
   },
   {
-    locationId: "calculus",
+    locationId: "Y78ufmYJvaCc0ZPE1UfW", //calculus
     x: 0.5,
     y: 0.1,
     initiallyHidden: true,
