9742f1a3084cc29fe678b0a351b79018a55a827f
oliverozcariz <oliverozcariz@britanico-aragon.edu>
2026-01-27 23:43:00 +0000

    public key set to private

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,12 +6,12 @@ import { getFirestore, collection, getDocs, doc, getDoc as getDocFirestore, upda
 // Your web app's Firebase configuration
 // IMPORTANT: In a real-world app, use environment variables for this sensitive data
 const firebaseConfig = {
-  apiKey: "AIzaSyAxoPOoeJSdYHD92RqYQNvXjlt8cVKptpE",
-  authDomain: "mathquest1-b6223.firebaseapp.com",
-  projectId: "mathquest1-b6223",
-  storageBucket: "mathquest1-b6223.firebasestorage.app",
-  messagingSenderId: "565631820722",
-  appId: "1:565631820722:web:ee17f44b05471f7b0dbaab"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
